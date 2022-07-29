e95f00ecdd2f804d6495f120c3307c7f808834cf
Parth Patel <p.patel81@yahoo.com>
2022-07-30 09:59:56 +1200

    Updates

---
+++ precache-manifest.1f71ced814e4ee83c4e70b3a4a0e1185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6150a92238440d1a1dd",
+    "url": "/www.keyuradroja.online/static/css/main.cd7f9772.chunk.css"
+  },
+  {
+    "revision": "a6150a92238440d1a1dd",
+    "url": "/www.keyuradroja.online/static/js/main.a6150a92.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/www.keyuradroja.online/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "e305a0e261f6ff5600ce",
+    "url": "/www.keyuradroja.online/static/js/2.e305a0e2.chunk.js"
+  },
+  {
+    "revision": "caac6c03405a71b28cf6",
+    "url": "/www.keyuradroja.online/static/js/runtime~main.caac6c03.js"
+  },
+  {
+    "revision": "3570a70e5997d6137704d66550c4ead7",
+    "url": "/www.keyuradroja.online/index.html"
+  }
+];
