cc8442a4986b7ffcbd3b7dfaa04e95ee13ceaa5d
Parth Patel <p.patel81@yahoo.com>
2022-07-31 12:14:19 +1200

    Updates

---
+++ precache-manifest.581ba0fe7fb02dcd176bc15ca5185ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6150a92238440d1a1dd",
+    "url": "/static/css/main.cd7f9772.chunk.css"
+  },
+  {
+    "revision": "a6150a92238440d1a1dd",
+    "url": "/static/js/main.a6150a92.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "e305a0e261f6ff5600ce",
+    "url": "/static/js/2.e305a0e2.chunk.js"
+  },
+  {
+    "revision": "ae42267ff6069bcb7427",
+    "url": "/static/js/runtime~main.ae42267f.js"
+  },
+  {
+    "revision": "ed8fa4b4fa45bcdeb67dabea35590a21",
+    "url": "/index.html"
+  }
+];
